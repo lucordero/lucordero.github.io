c2488dd38c38407a416b02021894f8f472aea964
Lucas Cordero <lucascordero1997@gmail.com>
2025-03-27 21:44:49 -0300

    Fix themetoggle button

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	darkMode: "class",
 	theme: {
 		extend: {
-			fontFamily:{
-				'hanken-grotesk': ['"Hanken Grotesk"', 'ui-sans-serif', 'system-ui']
+			fontFamily: {
+				"hanken-grotesk": [
+					'"Hanken Grotesk"',
+					"ui-sans-serif",
+					"system-ui",
+				],
 			},
-			screens:{
-				'xs':'400px',
+			screens: {
+				xs: "400px",
 			},
-			backgroundImage: theme=>({
-			'noisy-pattern': "url('/src/icons/noisy.webp')"
-			})
+			backgroundImage: (theme) => ({
+				"noisy-pattern": "url('/src/icons/noisy.webp')",
+			}),
 		},
 	},
 	plugins: [],
-}
+};
