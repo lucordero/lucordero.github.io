652815f3f0b5224feeb35cac7600b7ac24f64897
Lucas Cordero <lucascordero1997@gmail.com>
2025-03-27 21:24:37 -0300

    Change icons back to .svg for experimental astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-
-import tailwindcss from '@tailwindcss/vite';
-
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-  site:'https://lucascordero.dev.ar'
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	site: "https://lucascordero.dev.ar",
+	experimental: {
+		svg: {
+			mode: "sprite",
+		},
+	},
 });
